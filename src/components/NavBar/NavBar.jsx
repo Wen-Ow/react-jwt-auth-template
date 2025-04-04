e972059b7da4520bfa30524d43a1146fb5b6887f
Wen Ow <ow.wen2022@gmail.com>
2025-04-04 16:49:28 -0400

    completed auth requests in React

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,45 @@
-// src/components/NavBar/NavBar.jsx
+import { Link } from "react-router";
+import { useContext } from "react";
+
+import { UserContext } from "../../contexts/UserContext";
 
 const NavBar = () => {
+  const { user, setUser } = useContext(UserContext);
+
+  const handleSignOut = () => {
+    localStorage.removeItem("token");
+    setUser(null);
+  };
+
   return (
     <nav>
-      <p>Navbar contents will go here.</p>
+      {user ? (
+        <ul>
+          <li>Welcome, {user.username}</li>
+          <li>
+            <Link to="/">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/" onClick={handleSignOut}>
+              Sign Out
+            </Link>
+          </li>
+        </ul>
+      ) : (
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/sign-up">Sign Up</Link>
+          </li>
+          <li>
+            <Link to="/sign-in">Sign In</Link>
+          </li>
+        </ul>
+      )}
     </nav>
   );
 };
 
 export default NavBar;
-
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,18 @@
-// src/main.jsx
-
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router"; // add import for BrowserRouter
+import { BrowserRouter } from "react-router";
+
+import { UserProvider } from "./contexts/UserContext";
+
 import "./index.css";
 import App from "./App.jsx";
 
-// Wrap the App component with the BrowserRouter component to enable
-// enable route handling throughout your application.
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
     </BrowserRouter>
   </StrictMode>
 );
